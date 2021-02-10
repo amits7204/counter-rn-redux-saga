b6ae5e8dd178247a25f213181928f93c162d5c01
Amit Singh <cm8204@gmail.com>
2021-02-10 17:43:44 +0530

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+// Imports: Screens
+import Counter from './screens/Counter';
+
+// Imports: Redux Store
+import { store } from './store/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Counter />
+      </View>
+    </Provider>
   );
 }
 
